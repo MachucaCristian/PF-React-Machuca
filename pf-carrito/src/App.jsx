a250522ffdc66ed4237920eb25e5135ad80c7e63
Cristian Machuca <flaircristian@hotmail.com>
2022-09-22 19:22:22 -0300

    primer pre entrega

---
+++ pf-carrito/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Navbar from './component/Navbar/Navbar';
+import Carwidget from './component/CarWidget/carwidget';
+
+import "./App.css";
+const App = () => {
+    return (
+        <>
+        <Navbar/>
+    <Carwidget/>
+        </>
+    );
+}
+
+export default App;
+
---
+++ pf-carrito/src/component/CarWidget/itemlistcontainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ItemListContainer = () => {
+    return (
+        <>
+           <h4 className="card-title">Modelo</h4>
+      <p className="card-text">Pc Completa.</p> 
+      <button className="btn btn-secondary my-2 my-sm-0" type="submit">Mas Info</button>
+        </>
+    );
+}
+
+export default ItemListContainer;

28cb75c72a01ac157053312051b446b3a193a2ce
Cristian Machuca <flaircristian@hotmail.com>
2022-10-30 16:46:40 -0300

    error imagen

--- pf-carrito/src/component/App.jsx
+++ pf-carrito/src/component/App.jsx
@@ -15,7 +15,7 @@ import Home from "./content/ItemListContainer/Home";
 import Detalleproducto from "./content/ItemDetailContainer/ItemDetail";
 import Error404 from "./content/Error404/Error404";
 import Chekout from "./Chekout/Chekout";
-import Carritovacio from "./content/carrito/carritovacio";
+
 
 
 const App = () => {
@@ -31,7 +31,7 @@ const App = () => {
                     <Route path="/chekout" element={<Chekout/>}/>
                     <Route path="/item/:id" element={<Detalleproducto/>}/>
                     <Route path="/producto/:id" element={<Producto/>}/>
-                    <Route path="/carritovacio" element={<Carritovacio/>}/>
+                    
                     <Route path="/carritocompleto" element={<Carritocompleto/>}/>
                     <Route path="/contacto" element={<Contacto/>}/>
                     <Route path="/confirmaConsulta" element={<ConfirmaConsulta/>}/>
--- pf-carrito/src/component/content/carrito/carrito.jsx
+++ pf-carrito/src/component/content/carrito/carrito.jsx
@@ -1,7 +1,7 @@
 import React, {useContext, useEffect, useState} from 'react';
 import { CarritoContext } from './carritoContext';
 import { Link } from "react-router-dom";
-import Carritovacio from './carritovacio';
+
 
 const Carrito = () => {
     const {carrito, agregarProducto, quitarProducto, clearCarrito}  = useContext(CarritoContext)
@@ -37,7 +37,14 @@ const Carrito = () => {
     }, [carrito]);
     const app = (carrito.length != 0) ? <div className='row'> {carritoLocal} </div>  : <> 
     <div className="card text-center position-absolute top-50 start-50 translate-middle " style={{width: '74rem'}}>
-        <Carritovacio/>
+    <div className="card mb-3">
+                    <img src="././img/fondo.png" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">No existen Articulos en el Carrito</h5>
+                        
+                        <Link className="btn btn-primary" to={"/"}>Ir al Home</Link>
+                    </div>
+                    </div>
         </div>
 
   </>
